b76098dd8446e34b2ca81c638a91c5e0de230572
kubota.takanori <kubota.takanori@moneyforward.co.jp>
2018-04-09 15:52:37 +0900

    検証中

---
+++ src/main/java/holdings/ozaki/matsudak/ChromeHeadlessApplication.java
@@ -0,0 +1,34 @@
+//
+// Source code recreated from a .class file by IntelliJ IDEA
+// (powered by Fernflower decompiler)
+//
+
+package holdings.ozaki.matsudak;
+
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.Selenide;
+import org.openqa.selenium.By;
+import org.slf4j.bridge.SLF4JBridgeHandler;
+
+public class ChromeHeadlessApplication {
+    public ChromeHeadlessApplication() {
+    }
+
+    public static void main(String... args) {
+        Selenide.open("https://trade.bitpoint.co.jp/bpj-web/login");
+        Selenide.$(By.id("userid")).setValue("tomotaka.fujishiro@gmail.com");
+        Selenide.$(By.id("passwd")).setValue("Passw0rd");
+        Selenide.$(By.id("login_btn")).click();
+        System.out.println(Selenide.$(By.xpath("//*[@id='assets_table']/tbody/tr[1]/td[3]/span[2]")).getText());
+        Selenide.screenshot("bitpoint.co.jp");
+    }
+
+    static {
+        SLF4JBridgeHandler.removeHandlersForRootLogger();
+        SLF4JBridgeHandler.install();
+        Configuration.browser = "chrome";
+        Configuration.headless = true;
+        Configuration.reportsFolder = "build/reports";
+        Configuration.browserSize = "1024x768";
+    }
+}
